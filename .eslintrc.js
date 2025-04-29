dc643976fc815b7499562223df49656eca6fe0de
Harshit Jaiswal <harshitj183@gmail.com>
2025-04-29 22:07:36 +0530

    Fix ESLint errors and update GitHub workflow

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,25 @@
 module.exports = {
-    "env": {
-        "node": true,
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 12
-    },
-    "rules": {
-        "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
-        "no-console": "off",
-        "indent": ["warn", 2],
-        "semi": ["warn", "always"]
-    },
-    "ignorePatterns": [
-        "node_modules/",
-        "public/js/bootstrap.bundle.min.js"
-    ]
+  "env": {
+    "node": true,
+    "browser": true,
+    "commonjs": true,
+    "es2021": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 12
+  },
+  "globals": {
+    "bootstrap": "readonly"
+  },
+  "rules": {
+    "no-unused-vars": "warn",
+    "no-console": "off",
+    "indent": ["warn", 2],
+    "semi": ["warn", "always"]
+  },
+  "ignorePatterns": [
+    "node_modules/",
+    "public/js/bootstrap.bundle.min.js"
+  ]
 };
